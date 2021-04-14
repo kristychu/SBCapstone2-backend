08772ee6a6cebed73b0a87bafa1f2dd96387dd21
Kristy Chu <kristychu@kristys-mbp.lan>
2021-04-14 14:53:55 -0700

    update seed data

--- skincare-seed.sql
+++ skincare-seed.sql
@@ -1,13 +1,13 @@
 INSERT INTO users (username, password, first_name, last_name, email, profile_img)
 VALUES ('testuser',
-        '$2b$12$AZH7virni5jlTTiGgEg4zu3lSvAw68qVEfSIOjJ3RqtbJbdW/Oi5q',
+        '$2b$12$t96m01J2aSKVitMly2c.m.XSFtYzk2JJBeeaqaQTCPcjMNrVtSJP.',
         'Test',
         'User',
-        'joel@joelburton.com',
-        FALSE),
-       ('testadmin',
-        '$2b$12$AZH7virni5jlTTiGgEg4zu3lSvAw68qVEfSIOjJ3RqtbJbdW/Oi5q',
-        'Test',
-        'Admin!',
-        'joel@joelburton.com',
-        TRUE);
+        'testuser@email.com',
+        'https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fhu%2Fsearch%2Fimages%3Fk%3Ddefault%2Bprofile%2Bpicture&psig=AOvVaw3xPpQbSZD8DBr3WkqOcOxd&ust=1617822271727000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKDspuCn6u8CFQAAAAAdAAAAABAD'),
+       ('testuser2',
+        '$2b$12$lyVUS5a165KJQ4ZgWhSCv.3ZEGvrdqh8Gtsru28Zj7UE5GOKeQRkS',
+        'Test2',
+        'User2',
+        'testuser2@email.com'
+        );

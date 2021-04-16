3c5ff605411ec0ee345f6b2fd77664f6c7a2176b
Kristy Chu <kristychu@kristys-mbp.lan>
2021-04-16 12:10:17 -0700

    update User model and user routes with patch and delete methods

--- models/user.js
+++ models/user.js
@@ -23,15 +23,15 @@ class User {
   static async authenticate(username, password) {
     // try to find the user first
     const result = await db.query(
-          `SELECT username,
-                  password,
-                  first_name AS "firstName",
-                  last_name AS "lastName",
-                  email,
-                  profile_img AS "profileImg"
-           FROM users
-           WHERE username = $1`,
-        [username],
+      `SELECT username,
+              password,
+              first_name AS "firstName",
+              last_name AS "lastName",
+              email,
+              profile_img AS "profileImg"
+        FROM users
+        WHERE username = $1`,
+      [username]
     );
 
     const user = result.rows[0];
@@ -55,13 +55,19 @@ class User {
    * Throws BadRequestError on duplicates.
    **/
 
-  static async register(
-      { username, password, firstName, lastName, email, profileImg }) {
+  static async register({
+    username,
+    password,
+    firstName,
+    lastName,
+    email,
+    profileImg,
+  }) {
     const duplicateCheck = await db.query(
-          `SELECT username
+      `SELECT username
            FROM users
            WHERE username = $1`,
-        [username],
+      [username]
     );
 
     if (duplicateCheck.rows[0]) {
@@ -71,23 +77,19 @@ class User {
     const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
 
     const result = await db.query(
-          `INSERT INTO users
-           (username,
-            password,
-            first_name,
-            last_name,
-            email,
-            profile_img)
-           VALUES ($1, $2, $3, $4, $5, $6)
-           RETURNING username, first_name AS "firstName", last_name AS "lastName", email, profile_img AS "profileImg"`,
-        [
-          username,
-          hashedPassword,
-          firstName,
-          lastName,
-          email,
-          profileImg
-        ],
+      `INSERT INTO users (username,
+                          password,
+                          first_name,
+                          last_name,
+                          email,
+                          profile_img)
+        VALUES ($1, $2, $3, $4, $5, $6)
+        RETURNING username,
+                  first_name AS "firstName",
+                  last_name AS "lastName",
+                  email,
+                  profile_img AS "profileImg"`,
+      [username, hashedPassword, firstName, lastName, email, profileImg]
     );
 
     const user = result.rows[0];
@@ -100,15 +102,15 @@ class User {
    * Returns [{ username, first_name, last_name, email, is_admin }, ...]
    **/
 
-   static async findAll() {
+  static async findAll() {
     const result = await db.query(
-          `SELECT username,
+      `SELECT username,
                   first_name AS "firstName",
                   last_name AS "lastName",
                   email,
                   profile_img AS "profileImg"
            FROM users
-           ORDER BY username`,
+           ORDER BY username`
     );
 
     return result.rows;
@@ -122,16 +124,16 @@ class User {
    * Throws NotFoundError if user not found.
    **/
 
-   static async get(username) {
+  static async get(username) {
     const userRes = await db.query(
-          `SELECT username,
-                  first_name AS "firstName",
-                  last_name AS "lastName",
-                  email,
-                  profile_img AS "profileImg"
-           FROM users
-           WHERE username = $1`,
-        [username],
+      `SELECT username,
+              first_name AS "firstName",
+              last_name AS "lastName",
+              email,
+              profile_img AS "profileImg"
+        FROM users
+        WHERE username = $1`,
+      [username]
     );
 
     const user = userRes.rows[0];
@@ -139,17 +141,79 @@ class User {
     if (!user) throw new NotFoundError(`No user: ${username}`);
 
     const userStepsRes = await db.query(
-          `SELECT s.id,
-                  s.routine_step AS "routineStep",
-                  s.time_of_day AS "timeOfDay",
-                  s.product_id AS "productId"
-           FROM step AS s
-           WHERE s.username = $1`, [username]);
+      `SELECT s.id,
+              s.routine_step AS "routineStep",
+              s.time_of_day AS "timeOfDay",
+              s.product_id AS "productId"
+        FROM step AS s
+        WHERE s.username = $1`,
+      [username]
+    );
 
-    user.steps = userStepsRes.rows.map(s => s.id)
+    user.steps = userStepsRes.rows.map((s) => s.id);
     return user;
   }
-}
 
+  /** Update user data with `data`.
+   *
+   * This is a "partial update" --- it's fine if data doesn't contain
+   * all the fields; this only changes provided ones.
+   *
+   * Data can include:
+   *   { firstName, lastName, password, email, profileImg }
+   *
+   * Returns { username, firstName, lastName, email, profileImg }
+   *
+   * Throws NotFoundError if not found.
+   *
+   * WARNING: this function can set a new password.
+   * Callers of this function must be certain they have validated inputs to this
+   * or a serious security risks are opened.
+   */
+
+  static async update(username, data) {
+    if (data.password) {
+      data.password = await bcrypt.hash(data.password, BCRYPT_WORK_FACTOR);
+    }
+
+    const { setCols, values } = sqlForPartialUpdate(data, {
+      firstName: "first_name",
+      lastName: "last_name",
+      profileImg: "profile_img",
+    });
+    const usernameVarIdx = "$" + (values.length + 1);
+
+    const querySql = `UPDATE users 
+                    SET ${setCols} 
+                    WHERE username = ${usernameVarIdx} 
+                    RETURNING username,
+                              first_name AS "firstName",
+                              last_name AS "lastName",
+                              email,
+                              profile_img AS "profileImg"`;
+    const result = await db.query(querySql, [...values, username]);
+    const user = result.rows[0];
+
+    if (!user) throw new NotFoundError(`No user: ${username}`);
+
+    delete user.password;
+    return user;
+  }
+
+  /** Delete given user from database; returns undefined. */
+
+  static async remove(username) {
+    let result = await db.query(
+      `DELETE
+         FROM users
+         WHERE username = $1
+         RETURNING username`,
+      [username]
+    );
+    const user = result.rows[0];
+
+    if (!user) throw new NotFoundError(`No user: ${username}`);
+  }
+}
 
 module.exports = User;
--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,9 @@ const {
   ensureLoggedIn,
   ensureLoggedInAndCorrectUser,
 } = require("../middleware/auth");
+const { BadRequestError } = require("../expressError");
 const User = require("../models/user");
+const userUpdateSchema = require("../schemas/userUpdate.json");
 
 const router = express.Router();
 
@@ -42,6 +44,53 @@ router.get(
     try {
       const user = await User.get(req.params.username);
       return res.json({ user });
+    } catch (err) {
+      return next(err);
+    }
+  }
+);
+
+/** PATCH /[username] { user } => { user }
+ *
+ * Data can include:
+ *   { firstName, lastName, password, email, profileImg }
+ *
+ * Returns { username, firstName, lastName, email, profileImg }
+ *
+ * Authorization required: logged in and same-user-as-:username
+ **/
+
+router.patch(
+  "/:username",
+  ensureLoggedInAndCorrectUser,
+  async function (req, res, next) {
+    try {
+      const validator = jsonschema.validate(req.body, userUpdateSchema);
+      if (!validator.valid) {
+        const errs = validator.errors.map((e) => e.stack);
+        throw new BadRequestError(errs);
+      }
+
+      const user = await User.update(req.params.username, req.body);
+      return res.json({ user });
+    } catch (err) {
+      return next(err);
+    }
+  }
+);
+
+/** DELETE /[username]  =>  { deleted: username }
+ *
+ * Authorization required: logged in and same-user-as-:username
+ **/
+
+router.delete(
+  "/:username",
+  ensureLoggedInAndCorrectUser,
+  async function (req, res, next) {
+    try {
+      await User.remove(req.params.username);
+      return res.json({ deleted: req.params.username });
     } catch (err) {
       return next(err);
     }

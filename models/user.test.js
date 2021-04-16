ed598c3b57c91cc33026ff0a2b02c65fc660cbaf
Kristy Chu <kristychu@kristys-mbp.lan>
2021-04-16 13:51:07 -0700

    update model and route tests related to user

--- models/user.test.js
+++ models/user.test.js
@@ -142,3 +142,78 @@ describe("get", function () {
     }
   });
 });
+
+/************************************** update */
+
+describe("update", function () {
+  const updateData = {
+    firstName: "NewF",
+    lastName: "NewF",
+    email: "new@email.com",
+    profileImg: "Newprofileimguser1link",
+  };
+
+  test("works", async function () {
+    let user = await User.update("u1", updateData);
+    expect(user).toEqual({
+      username: "u1",
+      ...updateData,
+    });
+  });
+
+  test("works: set password", async function () {
+    let user = await User.update("u1", {
+      password: "new",
+    });
+    expect(user).toEqual({
+      username: "u1",
+      firstName: "U1F",
+      lastName: "U1L",
+      email: "u1@email.com",
+      profileImg: "profileimguser1link",
+    });
+    const found = await db.query("SELECT * FROM users WHERE username = 'u1'");
+    expect(found.rows.length).toEqual(1);
+    expect(found.rows[0].password.startsWith("$2b$")).toEqual(true);
+  });
+
+  test("not found if no such user", async function () {
+    try {
+      await User.update("nope", {
+        firstName: "test",
+      });
+      fail();
+    } catch (err) {
+      expect(err instanceof NotFoundError).toBeTruthy();
+    }
+  });
+
+  test("bad request if no data", async function () {
+    expect.assertions(1);
+    try {
+      await User.update("u1", {});
+      fail();
+    } catch (err) {
+      expect(err instanceof BadRequestError).toBeTruthy();
+    }
+  });
+});
+
+/************************************** remove */
+
+describe("remove", function () {
+  test("works", async function () {
+    await User.remove("u1");
+    const res = await db.query("SELECT * FROM users WHERE username='u1'");
+    expect(res.rows.length).toEqual(0);
+  });
+
+  test("not found if no such user", async function () {
+    try {
+      await User.remove("nope");
+      fail();
+    } catch (err) {
+      expect(err instanceof NotFoundError).toBeTruthy();
+    }
+  });
+});
--- routes/users.test.js
+++ routes/users.test.js
@@ -4,6 +4,7 @@ const request = require("supertest");
 
 const db = require("../db.js");
 const app = require("../app");
+const User = require("../models/user");
 
 const {
   commonBeforeAll,
@@ -99,6 +100,98 @@ describe("GET /users/:username", function () {
     const resp = await request(app)
       .get(`/api/users/nope`)
       .set("authorization", `Bearer ${u1Token}`);
+    expect(resp.statusCode).toEqual(401);
+  });
+});
+
+/************************************** PATCH /users/:username */
+
+describe("PATCH /users/:username", () => {
+  test("works for same user", async function () {
+    const resp = await request(app)
+      .patch(`/api/users/u1`)
+      .send({
+        firstName: "New",
+      })
+      .set("authorization", `Bearer ${u1Token}`);
+    expect(resp.body).toEqual({
+      user: {
+        username: "u1",
+        firstName: "New",
+        lastName: "U1L",
+        email: "user1@user.com",
+        profileImg: "profileimguser1link",
+      },
+    });
+  });
+
+  test("unauth if not same user", async function () {
+    const resp = await request(app)
+      .patch(`/api/users/u1`)
+      .send({
+        firstName: "New",
+      })
+      .set("authorization", `Bearer ${u2Token}`);
+    expect(resp.statusCode).toEqual(401);
+  });
+
+  test("unauth for anon", async function () {
+    const resp = await request(app).patch(`/api/users/u1`).send({
+      firstName: "New",
+    });
+    expect(resp.statusCode).toEqual(401);
+  });
+
+  test("bad request if invalid data", async function () {
+    const resp = await request(app)
+      .patch(`/api/users/u1`)
+      .send({
+        firstName: 42,
+      })
+      .set("authorization", `Bearer ${u1Token}`);
+    expect(resp.statusCode).toEqual(400);
+  });
+
+  test("works: can set new password", async function () {
+    const resp = await request(app)
+      .patch(`/api/users/u1`)
+      .send({
+        password: "new-password",
+      })
+      .set("authorization", `Bearer ${u1Token}`);
+    expect(resp.body).toEqual({
+      user: {
+        username: "u1",
+        firstName: "U1F",
+        lastName: "U1L",
+        email: "user1@user.com",
+        profileImg: "profileimguser1link",
+      },
+    });
+    const isSuccessful = await User.authenticate("u1", "new-password");
+    expect(isSuccessful).toBeTruthy();
+  });
+});
+
+/************************************** DELETE /users/:username */
+
+describe("DELETE /users/:username", function () {
+  test("works for same user", async function () {
+    const resp = await request(app)
+      .delete(`/api/users/u1`)
+      .set("authorization", `Bearer ${u1Token}`);
+    expect(resp.body).toEqual({ deleted: "u1" });
+  });
+
+  test("unauth if not same user", async function () {
+    const resp = await request(app)
+      .delete(`/api/users/u1`)
+      .set("authorization", `Bearer ${u2Token}`);
+    expect(resp.statusCode).toEqual(401);
+  });
+
+  test("unauth for anon", async function () {
+    const resp = await request(app).delete(`/api/users/u1`);
     expect(resp.statusCode).toEqual(401);
   });
 });

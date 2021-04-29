26481a95f384ec80be2b697f9b946dbf4abfe4ca
Kristy Chu <kristychu@kristys-mbp.lan>
2021-04-28 22:43:15 -0700

    revert back - take out CORS config

--- app.js
+++ app.js
@@ -25,11 +25,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", usersRoutes);
 app.use("/api/steps", stepsRoutes);
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
-
 /** Handle 404 errors -- this matches everything */
 app.use(function (req, res, next) {
   return next(new NotFoundError());
